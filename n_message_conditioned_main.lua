d13e1bb2c9a906925b9d84c21a47b2a659257991
Arnab Ghosh <arnabg@ttic.edu>
2016-12-31 13:46:13 +0530

    Code Running But Some Issue in training

--- n_message_conditioned_main.lua
+++ n_message_conditioned_main.lua
@@ -4,25 +4,25 @@ require 'optim'
 require 'rnn'
 local model_utils = require 'util.model_utils'
 opt = {
-   dataset = 'lsun',       -- imagenet / lsun / folder
-   batchSize = 64,
-   loadSize = 96,
-   fineSize = 64,
-   nmsg = 50,              -- #  of dim for the message
-   nz = 100,               -- #  of dim for Z
-   ngf = 64,               -- #  of gen filters in first conv layer
-   ndf = 64,               -- #  of discrim filters in first conv layer
-   nThreads = 4,           -- #  of data loading threads to use
-   niter = 25,             -- #  of iter at starting learning rate
-   lr = 0.0002,            -- initial learning rate for adam
-   beta1 = 0.5,            -- momentum term of adam
-   ntrain = math.huge,     -- #  of examples per epoch. math.huge for full dataset
-   display = 1,            -- display samples while training. 0 = false
-   display_id = 20,        -- display window id.
-   gpu = 1,                -- gpu = 0 is CPU mode. gpu=X is GPU mode on GPU X
-   name = 'experiment-message_conditioned1',
-   noise = 'normal',       -- uniform / normal
-   ngen = 2,               -- the number of generators generating images                
+    dataset = 'lsun',       -- imagenet / lsun / folder
+    batchSize = 64,
+    loadSize = 96,
+    fineSize = 64,
+    nmsg = 50,              -- #  of dim for the message
+    nz = 100,               -- #  of dim for Z
+    ngf = 64,               -- #  of gen filters in first conv layer
+    ndf = 64,               -- #  of discrim filters in first conv layer
+    nThreads = 4,           -- #  of data loading threads to use
+    niter = 25,             -- #  of iter at starting learning rate
+    lr = 0.0002,            -- initial learning rate for adam
+    beta1 = 0.5,            -- momentum term of adam
+    ntrain = math.huge,     -- #  of examples per epoch. math.huge for full dataset
+    display = 1,            -- display samples while training. 0 = false
+    display_id = 20,        -- display window id.
+    gpu = 1,                -- gpu = 0 is CPU mode. gpu=X is GPU mode on GPU X
+    name = 'experiment-n_message_conditioned1',
+    noise = 'normal',       -- uniform / normal
+    ngen = 2,               -- the number of generators generating images                
 }
 
 -- one-line argument parser. parses enviroment variables to override the defaults
@@ -42,14 +42,14 @@ local data = DataLoader.new(opt.nThreads, opt.dataset, opt)
 print("Dataset: " .. opt.dataset, " Size: ", data:size())
 ----------------------------------------------------------------------------
 local function weights_init(m)
-   local name = torch.type(m)
-   if name:find('Convolution') then
-      m.weight:normal(0.0, 0.02)
-      m:noBias()
-   elseif name:find('BatchNormalization') then
-      if m.weight then m.weight:normal(1.0, 0.02) end
-      if m.bias then m.bias:fill(0) end
-   end
+    local name = torch.type(m)
+    if name:find('Convolution') then
+        m.weight:normal(0.0, 0.02)
+        m:noBias()
+    elseif name:find('BatchNormalization') then
+        if m.weight then m.weight:normal(1.0, 0.02) end
+        if m.bias then m.bias:fill(0) end
+    end
 end
 
 local nc = 3
@@ -128,10 +128,10 @@ for i=2,ngen do
     G['netM' .. i]:apply(weights_init)
 end
 
-G.Reducer=nn.Sequential()
-G.Reducer:add(nn.SplitTable(1,2))
-G.Reducer:add(nn.Sequencer(nn.LSTM( nmsg , nmsg )  ))
-G.Reducer:add(nn.SelectTable(-1))
+G.reducer=nn.Sequential()
+G.reducer:add(nn.SplitTable(1,2))
+G.reducer:add(nn.Sequencer(nn.LSTM( nmsg , nmsg )  ))
+G.reducer:add(nn.SelectTable(-1))
 
 local netD = nn.Sequential()
 
@@ -159,12 +159,12 @@ netD:apply(weights_init)
 local criterion = nn.BCECriterion()
 ---------------------------------------------------------------------------
 optimStateG = {
-   learningRate = opt.lr,
-   beta1 = opt.beta1,
+    learningRate = opt.lr,
+    beta1 = opt.beta1,
 }
 optimStateD = {
-   learningRate = opt.lr,
-   beta1 = opt.beta1,
+    learningRate = opt.lr,
+    beta1 = opt.beta1,
 }
 ----------------------------------------------------------------------------
 local input = torch.Tensor(opt.batchSize, 3, opt.fineSize, opt.fineSize)
@@ -183,20 +183,20 @@ local prev_fake_cache = torch.Tensor(ngen,opt.batchSize, 3, opt.fineSize, opt.fi
 local provisional_message_cache = torch.Tensor(ngen,opt.batchSize,nmsg,1,1):normal(0,1)
 ----------------------------------------------------------------------------
 if opt.gpu > 0 then
-   require 'cunn'
-   cutorch.setDevice(opt.gpu)
-   input = input:cuda();  noise = noise:cuda();  noise_cache= noise_cache:cuda(); label = label:cuda() ; message=message:cuda() ; prev_message = prev_message:cuda() ; prev_fake_cache = prev_fake_cache:cuda() ; provisional_message_cache=provisional_message_cache:cuda();prev_noise_cache=prev_noise_cache:cuda();prev_mapped_message_cache=prev_mapped_message_cache:cuda()
+    require 'cunn'
+    cutorch.setDevice(opt.gpu)
+    input = input:cuda();  noise = noise:cuda();  noise_cache= noise_cache:cuda(); label = label:cuda() ; message=message:cuda() ; prev_message = prev_message:cuda() ; prev_fake_cache = prev_fake_cache:cuda() ; provisional_message_cache=provisional_message_cache:cuda();prev_noise_cache=prev_noise_cache:cuda();prev_mapped_message_cache=prev_mapped_message_cache:cuda()
 
---   if pcall(require, 'cudnn') then
---      require 'cudnn'
---      cudnn.benchmark = true
---      --cudnn.convert(netG, cudnn)
---      for k,net in pairs(G) do cudnn.convert(net,cudnn)  end
---      cudnn.convert(netD, cudnn)
---   end
-   netD:cuda();           --netG:cuda();          
-   criterion:cuda()
-   for k,net in pairs(G) do net:cuda() end
+    --   if pcall(require, 'cudnn') then
+    --      require 'cudnn'
+    --      cudnn.benchmark = true
+    --      --cudnn.convert(netG, cudnn)
+    --      for k,net in pairs(G) do cudnn.convert(net,cudnn)  end
+    --      cudnn.convert(netD, cudnn)
+    --   end
+    netD:cuda();           --netG:cuda();          
+    criterion:cuda()
+    for k,net in pairs(G) do net:cuda() end
 end
 
 
@@ -219,7 +219,7 @@ for i=1,ngen do
     G['netI'..i]:forward(prev_fake_cache[i])
     G['netM'..i]:forward( torch.cat({ provisional_message_cache[i]:reshape(opt.batchSize,nmsg), noise_cache[i]:reshape(opt.batchSize,nz-nmsg ) , message:reshape(opt.batchSize,nmsg)  }  ,2 )  )
 end
-reducer:forward(prev_mapped_message_cache)
+G.reducer:forward(prev_mapped_message_cache)
 
 ----------------------------------------------------------------
 
@@ -227,118 +227,120 @@ reducer:forward(prev_mapped_message_cache)
 -- create closure to evaluate f(X) and df/dX of discriminator
 
 local fDx = function(x)
-   gradParametersD:zero()
-   -- Currently 1 batch of real data and 1 batch each from the generators
-   -- Might try a version in which n batches of real data along with 1 batch each from the generators
-   -- train with real
-   data_tm:reset(); data_tm:resume()
-   local real = data:getBatch()
-   data_tm:stop()
-   input:copy(real)
-   label:fill(real_label)
+    gradParametersD:zero()
+    -- Currently 1 batch of real data and 1 batch each from the generators
+    -- Might try a version in which n batches of real data along with 1 batch each from the generators
+    -- train with real
+    data_tm:reset(); data_tm:resume()
+    local real = data:getBatch()
+    data_tm:stop()
+    input:copy(real)
+    label:fill(real_label)
 
-   local output = netD:forward(input)
-   local errD = criterion:forward(output, label)
-   local df_do = criterion:backward(output, label)
-   netD:backward(input, df_do)
+    local output = netD:forward(input)
+    local errD = criterion:forward(output, label)
+    local df_do = criterion:backward(output, label)
+    netD:backward(input, df_do)
 
-   for i=1,ngen do
-      if opt.noise == 'uniform' then 
-          noise:uniform(-1,1)
-      elseif opt.noise=='normal' then
-          noise:normal(0,1)
-      end
-      noise_cache[i]=noise
-      local fake=G['netG'..i]:forward( torch.cat( noise,message,2))
-      input:copy(fake)
-      label:fill(fake_label)
+    for i=1,ngen do
+        if opt.noise == 'uniform' then 
+            noise:uniform(-1,1)
+        elseif opt.noise=='normal' then
+            noise:normal(0,1)
+        end
+        noise_cache[i]=noise
+        local fake=G['netG'..i]:forward( torch.cat( noise,message,2))
+        input:copy(fake)
+        label:fill(fake_label)
 
-      local output=netD:forward(input)
-      errD=errD+criterion:forward(output,label)
-      local df_do = criterion:backward(output,label)
-      netD:backward(input,df_do)
-   end
-   return errD,gradParametersD 
+        local output=netD:forward(input)
+        errD=errD+criterion:forward(output,label)
+        local df_do = criterion:backward(output,label)
+        netD:backward(input,df_do)
+    end
+    return errD,gradParametersD 
 end
 
 -- create closure to evaluate f(X) and df/dX of generator
-local fGx = function(x) do
-   gradParametersG:zero()
-   label:fill(real_label)
-   local errG=0
-   local df_message = torch.Tensor( opt.batchSize , nmsg , 1, 1 ):fill(0)
-   for i=1,ngen do
-      local output = netD:forward(G['netG' .. i].output)
-      errG = errG + criterion:forward(output,label)
-      local df_do = criterion:backward(output,label)
+local fGx = function(x)
+    gradParametersG:zero()
+    label:fill(real_label)
+    local errG=0
+    local df_message = torch.Tensor( opt.batchSize , nmsg , 1, 1 ):fill(0)
+    df_message=df_message:cuda()
+    for i=1,ngen do
+        local output = netD:forward(G['netG' .. i].output)
+        errG = errG + criterion:forward(output,label)
+        local df_do = criterion:backward(output,label)
 
-      local df_dg = netD:updateGradInput(G['netG' .. i].output,df_do)
+        local df_dg = netD:updateGradInput(G['netG' .. i].output,df_do)
 
-      local df_input=G['netG'..i]:backward(torch.cat(noise_cache[i],message,2),df_dg)
-      df_message=df_message+df_input[{ {} ,{nz-nmsg+1,nz}}]  
-   end
-   df_mapped_messages=reducer:backward( prev_mapped_message_cache, df_message:reshape(opt.batchSize,nmsg))
-   df_mapped_messages=df_mapped_messages:transpose(1,2)
-   for i=1,ngen do
-       local df_dM = G['netM'..i]:backward(torch.cat( { provisional_message_cache[i]:reshape(opt.batchSize,nmsg),prev_noise_cache[i]:reshape(opt.batchSize,nz-nmsg ) , prev_message:reshape(opt.batchSize,nmsg)  },2) , df_mapped_messages[i]:reshape(opt.batchSize,nmsg))
-       local df_dI = df_dM[ { {} , { 1 , nmsg  } }]:reshape(opt.batchSize,nmsg,1,1)
-       G['netI'..i]:backward( prev_fake_cache[i] , df_dI )
-   end
+        local df_input=G['netG'..i]:backward(torch.cat(noise_cache[i],message,2),df_dg)
+        df_message=df_message+df_input[{ {} ,{nz-nmsg+1,nz}}]  
+    end
+    df_mapped_messages=G.reducer:backward( prev_mapped_message_cache, df_message:reshape(opt.batchSize,nmsg))
+    df_mapped_messages=df_mapped_messages:transpose(1,2)
+    for i=1,ngen do
+        local df_dM = G['netM'..i]:backward(torch.cat( { provisional_message_cache[i]:reshape(opt.batchSize,nmsg),prev_noise_cache[i]:reshape(opt.batchSize,nz-nmsg ) , prev_message:reshape(opt.batchSize,nmsg)  },2) , df_mapped_messages[i]:reshape(opt.batchSize,nmsg))
+        local df_dI = df_dM[ { {} , { 1 , nmsg  } }]:reshape(opt.batchSize,nmsg,1,1)
+        G['netI'..i]:backward( prev_fake_cache[i] , df_dI )
+    end
 
-   -- Forward Processing of the messages and the reducer and the filling up of the caches need to be updated
-   local temp_prev_mapped_message_cache=torch.Tensor(ngen,opt.batchSize,nmsg)
-   for i=1,ngen do
-       provisional_message_cache[i]=G['netI'..i]:forward(G['netG'..i].output)
-       temp_prev_mapped_message_cache[i]=G['netM'..i]:forward( torch.cat({ provisional_message_cache[i]:reshape(opt.batchSize,nmsg), noise_cache[i]:reshape(opt.batchSize,nz-nmsg ) , message:reshape(opt.batchSize,nmsg)  }  ,2 )  )
-       prev_fake_cache[i]=G['netG'..i].output
-   end
-   prev_mapped_message_cache=temp_prev_mapped_message_cache:transpose(1,2)
-   prev_message=message
-   message=reducer:forward(prev_mapped_message_cache)
-   prev_noise_cache=noise_cache
-   return errG, gradParametersG
+    -- Forward Processing of the messages and the reducer and the filling up of the caches need to be updated
+    local temp_prev_mapped_message_cache=torch.Tensor(ngen,opt.batchSize,nmsg):cuda()
+    for i=1,ngen do
+        provisional_message_cache[i]=G['netI'..i]:forward(G['netG'..i].output)
+        temp_prev_mapped_message_cache[i]=G['netM'..i]:forward( torch.cat({ provisional_message_cache[i]:reshape(opt.batchSize,nmsg), noise_cache[i]:reshape(opt.batchSize,nz-nmsg ) , message:reshape(opt.batchSize,nmsg)  }  ,2 )  )
+        prev_fake_cache[i]=G['netG'..i].output
+    end
+    prev_mapped_message_cache=temp_prev_mapped_message_cache:transpose(1,2)
+    prev_message=message
+    message=G.reducer:forward(prev_mapped_message_cache)
+    prev_noise_cache=noise_cache
+    return errG, gradParametersG
 end
 
 -- train
 for epoch = 1, opt.niter do
-   epoch_tm:reset()
-   local counter = 0
-   for i = 1, math.min(data:size(), opt.ntrain), opt.batchSize do
-      tm:reset()
-      -- (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-      optim.adam(fDx, parametersD, optimStateD)
+    epoch_tm:reset()
+    local counter = 0
+    for i = 1, math.min(data:size(), opt.ntrain), opt.batchSize do
+        tm:reset()
+        -- (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        optim.adam(fDx, parametersD, optimStateD)
 
-      -- (2) Update G network: maximize log(D(G(z)))
-      optim.adam(fGx, parametersG, optimStateG)
+        -- (2) Update G network: maximize log(D(G(z)))
+        optim.adam(fGx, parametersG, optimStateG)
 
-      -- display
-      counter = counter + 1
-      if counter % 10 == 0 and opt.display then
-          local real = data:getBatch()
-          disp.image(real,  {win=opt.display_id , title=opt.name})
-          for i=1,ngen do
-              local fake=G['netG'..i]:forward(torch.cat(noise_vis,message,2))
-              disp.image(fake, {win=opt.display_id+i,title=opt.name })
-          end
-      end
+        -- display
+        counter = counter + 1
+        if counter % 10 == 0 and opt.display then
+            local real = data:getBatch()
+            disp.image(real,  {win=opt.display_id , title=opt.name})
+            for i=1,ngen do
+                local fake=G['netG'..i]:forward(torch.cat(noise_vis,message,2))
+                disp.image(fake, {win=opt.display_id+i,title=opt.name })
+            end
+        end
 
-      -- logging
-      if ((i-1) / opt.batchSize) % 1 == 0 then
-         print(('Epoch: [%d][%8d / %8d]\t Time: %.3f  DataTime: %.3f  '
-                   .. '  Err_G: %.4f  Err_D: %.4f'):format(
-                 epoch, ((i-1) / opt.batchSize),
-                 math.floor(math.min(data:size(), opt.ntrain) / opt.batchSize),
-                 tm:time().real, data_tm:time().real,
-                 errG and errG or -1, errD and errD or -1))
-      end
-   end
-   paths.mkdir('checkpoints_message_conditioned')
-   --parametersD, gradParametersD = nil, nil -- nil them to avoid spiking memory
-   --parametersG, gradParametersG = nil, nil
-   torch.save('checkpoints_message_conditioned/' .. opt.name .. '_' .. epoch .. '_net_G.t7', {G=G,message=message} )
-   torch.save('checkpoints_message_conditioned/' .. opt.name .. '_' .. epoch .. '_net_D.t7', netD )
-   --parametersD, gradParametersD = netD:getParameters() -- reflatten the params and get them
-   --parametersG, gradParametersG = netG:getParameters()
-   --parametersG, gradParametersG = model_utils.combine_all_parameters(G)
-   print(('End of epoch %d / %d \t Time Taken: %.3f'):format(
-            epoch, opt.niter, epoch_tm:time().real))
+        -- logging
+        if ((i-1) / opt.batchSize) % 1 == 0 then
+            print(('Epoch: [%d][%8d / %8d]\t Time: %.3f  DataTime: %.3f  '
+            .. '  Err_G: %.4f  Err_D: %.4f'):format(
+            epoch, ((i-1) / opt.batchSize),
+            math.floor(math.min(data:size(), opt.ntrain) / opt.batchSize),
+            tm:time().real, data_tm:time().real,
+            errG and errG or -1, errD and errD or -1))
+        end
+    end
+    paths.mkdir('checkpoints_n_message_conditioned')
+    --parametersD, gradParametersD = nil, nil -- nil them to avoid spiking memory
+    --parametersG, gradParametersG = nil, nil
+    torch.save('checkpoints_n_message_conditioned/' .. opt.name .. '_' .. epoch .. '_net_G.t7', {G=G,message=message} )
+    torch.save('checkpoints_n_message_conditioned/' .. opt.name .. '_' .. epoch .. '_net_D.t7', netD )
+    --parametersD, gradParametersD = netD:getParameters() -- reflatten the params and get them
+    --parametersG, gradParametersG = netG:getParameters()
+    --parametersG, gradParametersG = model_utils.combine_all_parameters(G)
+    print(('End of epoch %d / %d \t Time Taken: %.3f'):format(
+    epoch, opt.niter, epoch_tm:time().real))
+end
